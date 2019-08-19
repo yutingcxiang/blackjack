0f42614427d1fd38e2456cbe6f77c01af9ad9455
Christine Xiang <christine@defmethod.io>
2019-08-19 12:13:00 -0400

    Add test for checking deck values

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,9 +3,10 @@ require_relative '../lib/deck.rb'
 
 RSpec.describe Deck do
   let(:new_deck) { Deck.new }
-  let(:new_deck_suits) { new_deck.cards.map {|card| card.suit} }
+  let(:new_deck_suits) { new_deck.cards.map(&:suit) }
+  let(:new_deck_values) { new_deck.cards.map(&:value) }
 
-  context 'wben Deck is initialized' do
+  context 'when Deck is initialized' do
     it 'will have 52 cards' do
       expect(new_deck.cards.count).to eq(52)
     end
@@ -16,6 +17,9 @@ RSpec.describe Deck do
       expect(new_deck_suits).to include('spades')
       expect(new_deck_suits).to include('clubs')
     end
+
+    it 'will contain 13 different values' do
+      expect(new_deck_values.uniq.count).to eq(13) 
+    end
   end
-    
 end
