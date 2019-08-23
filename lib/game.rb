ea1ae116fb91e744a80b44f36694e7cb6b2b29cc
Christine Xiang <christine@defmethod.io>
2019-08-23 10:25:22 -0400

    Add code and tests for game.setup

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_reader :deck, :player, :dealer
+  attr_accessor :deck, :player, :dealer
 
   def initialize
     @deck = Deck.new
@@ -9,5 +9,15 @@ class Game
 
   def setup
     @deck.shuffle
+
+    card1 = @deck.deal
+    @player.hit(card1)
+    card2 = @deck.deal
+    @player.hit(card2)
+
+    card3 = @deck.deal
+    @dealer.hit(card3)
+    card4 = @deck.deal
+    @dealer.hit(card4)
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,8 +9,6 @@ RSpec.describe "Game" do
   let(:new_game) { Game.new }
 
   context "initializes new game" do
-    let(:new_game) { Game.new }
-
     it 'initializes a new deck' do
       expect(new_game.deck).to be_an_instance_of Deck
     end
@@ -22,25 +20,22 @@ RSpec.describe "Game" do
     it 'initializes a new dealer' do
       expect(new_game.dealer).to be_an_instance_of Dealer
     end
+  end
 
-    describe '#setup' do
-      it 'shuffles deck' do
-        expect(new_game.deck).to receive(shuffle)
-        new_game.setup
-      end
-
-      it 'deals two cards to the player and to the dealer' do
-        expect(new_game.player).to receive(hit).twice
-        expect(new_game.dealer).to receive(hit).twice
-      end
-
-      it 'reveals the first card from the dealer' do
-        expect(new_game.dealer).to receive(hit).twice
-      end
+  describe '#setup' do
+    it 'shuffles deck' do
+      expect(new_game.deck).to receive(:shuffle)
+      new_game.setup
     end
+
+    it 'deals two cards to the player and to the dealer' do
+      expect(new_game.player).to receive(:hit).twice
+      expect(new_game.dealer).to receive(:hit).twice
+      new_game.setup
+    end
+  end
 
     # game_over? -> t/f
     # next_turn?
     # current_player
-  end
 end
